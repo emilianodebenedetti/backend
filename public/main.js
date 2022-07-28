0b7f03a85405288fee3a3524275e4f072307ccc4
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2022-07-27 22:42:15 -0300

    chatApp-debenedetti

--- public/main.js
+++ public/main.js
@@ -1,16 +1,32 @@
-const { render } = require("ejs");
+/* const { render } = require("ejs"); */
+//const socket = io(); //websockets
+const socket = io.connect(); 
+let idSocket = 0;
+function setSocketId(id){
+    idSocket = id;
+}
 
-let socket = io.connect();
-socket.on('messages', function(data) {
+const input = document.querySelector('input')
+document.querySelector('button').addEventListener('click', () => {
+    socket.emit('message', input.value);
+})
+
+socket.on('messages', data => { //, function(data)
+    const mensajesHTML = data
+    .map(msj => `SocketId: ${msj.socketid} -> Mensaje: ${msj.message}`)
+    .join('<br>')
     console.log(data);
     render(data);
+    document.querySelector('p').innerHTML = mensajesHTML
 });
 
 function render(data) {
     let html = data.map(function(elem, index){
         return(`<div>
                 <strong>${elem.author}</strong>
-                <em>${elem.text}</em> </div>`)
+                <em>${elem.text}</em> 
+                </div>
+                `)
     }).join(" ");
     document.getElementById('messages').innerHTML = html;
 }
@@ -22,7 +38,7 @@ function addMessage() {
     };
     socket.emit('new-message', mensaje); //nombre evento new-message
 
-    document.getElementById('texto').value = '' //vaciar campo de texto(no recargamos pagina)
-    document.getElementById('texto').focus()
+    document.getElementById('text').value = '' //vaciar campo de texto(no recargamos pagina)
+    document.getElementById('text').focus()
     
 }
--- server.js
+++ server.js
@@ -1,46 +1,71 @@
 const express = require('express')
+/* const server = require('http').Server(app) */
+/* const io = require('socket.io')(server) */
+const { Server: HttpServer } = require('http')     
+const { Server: IOServer } = require('socket.io')  
+/* const handlebars = require("express-handlebars"); */   //cargamos modulo handlebars
+
+//socket.io
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
 
-const productos = [];
-
-
+//establecemos config de handlebars
+/* app.engine(
+    "hbs",
+    handlebars({
+        extname: ".hbs",                            
+        defaultLayout: 'index.hbs',
+        layout: __dirname + "/views/layouts",
+        partialsDir: __dirname + '/views/partials'
+    })
+); */
+//establecemos motor d eplantilla a utilizar
+/* app.set("view engine", "hbs"); */
+//establecemos directorio donde se encuentran los archivos de plantilla
+app.set("views", "./views");
+//espacio publico del servidor
+app.use(express.static('public'))
+app.use(express.static('./public')) //archivos estaticos q se encuentran aca
 app.use(express.urlencoded({ extended: true }))
-
 app.set('views','./views');
 app.set('view engine','ejs');
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.render('inicio', { productos });
+});  */
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root:__dirname});
+    
 });
+
+const productos = [];
 
 app.post('/productos', (req, res) => {
     productos.push(req.body)
     console.log(productos)
     res.redirect('/')
 });
-
 // socket.io
-let messages = [
-    
-];
-
-app.use(express.static('public'));
-
-io.on('connection', function(socket) {
+let messages = [];
+//Cada vez q se abra la app ...
+io.on('connection', socket => { //io.on('connection', function(socket) {...}                             --- (websockets)
     console.log('Un cliente se ha conectado');
+
     socket.emit('messages', messages); //emitir todos los mensajes a un cliente nuevo
 
-    socket.on('new-message', function(data){
-        messages.push(data); //agregamos msjs al array
-        io.socket.emit('messages', messages); //emitir a todos los clientes
-    });
+    socket.on('message', data => { //, function(data)
+        messages.push({ socketid: socket.id, message: data }) //messages.push(data);
+        io.sockets.emit('messages', messages); //emitir a todos los clientes
+    })
 });
 
 const PORT = process.env.PORT || 8080;
 
-const srv = server.listen(PORT, () => { //server.listen por socket.io, sino app.listen
+const srv = httpServer.listen(PORT, () => { 
     console.log(`Servidor escuchando en el puerto ${srv.address().port}`)
 })
 srv.on('error', error => console.log(`Error en el servidor ${error}`))
+/* io.on('connection', (socket) => {
+    console.log('Usuario conectado')
+}) */
