4fdc9972ee3f553a7f9170f664a15f6090f4dd08
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2022-07-28 11:49:26 -0300

    1er-preentrega-debenedetti

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ const PORT = 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor conectado escuchando en el puerto ${server.address().port}`)
 })
-server.on('error', error => console-log(`Error en servidor ${error}`))
+server.on('error', error => console.log(`Error en servidor ${error}`))
